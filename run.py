49bbe5ade645c9e4818cfd075790d645f5311781
ik <ian.kingori@andela.com>
2017-09-12 18:35:31 +0300

    (feat) add events and messages routes

---
+++ run.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+from ast import literal_eval
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, request, Response
+from slackclient import SlackClient
+
+import services
+
+load_dotenv(find_dotenv())
+# Flask Variables
+HOST = os.environ.get('HOST') or '127.0.0.1'
+PORT = os.environ.get('PORT') or 4040
+
+# Slack Variables
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+slack_client = SlackClient(BOT_TOKEN)
+BOT = slack_client.api_call('auth.test')
+
+print(BOT)
+
+app = Flask(__name__)
+
+incident_category = None
+
+@app.route('/events', methods=['GET', 'POST'])
+def events():
+    global incident_category
+    challenge = request.json.get('challenge')
+    if challenge: 
+        return Response(json.dumps({'challenge': challenge}))
+    elif incident_category is None:
+        services.dispatch(request.json)
+        return Response(response='ok', status=200, headers={'X-Slack-No-Retry': 1})
+    else:
+        print(request.json)
+
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    global incident_category
+    data = json.loads(request.form['payload'])
+    callback_id = data[u'callback_id']
+    if callback_id == 'category':
+        incident_category = data['actions'][0][u'selected_options'][0][u'value']
+    return Response(response='Describe the incident', 
+                    status=200, 
+                    headers={'X-Slack-No-Retry': 1}
+                    )
+
+
+def main():
+    app.debug=True
+    app.run(HOST, int(PORT))
+
+if __name__ == "__main__":
+    main()
