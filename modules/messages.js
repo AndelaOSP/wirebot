287c0ab4cbc6097e707b972cfad4d27b2a716815
ik <ian.kingori@andela.com>
2017-10-19 00:12:09 +0300

    (feat #151797578) log incidents via slack - add date - add category - add description - add location - tag witnesses

--- modules/messages.js
+++ modules/messages.js
@@ -3,13 +3,133 @@ const initiate = {
     {
       color: '#5A352D',
       title: 'Do you want to report an incident?',
-      callback_id: 'log',
+      callback_id: 'report',
       actions: [
         {
           name: 'yes',
           text: 'Yes',
           type: 'button',
-          value: 'report',
+          value: 'yes',
+        },
+        {
+          name: 'no',
+          text: 'No',
+          type: 'button',
+          value: 'no',
+        },
+      ],
+    },
+  ],
+};
+
+const categoryMessage = {
+  attachments: [
+    {
+      "color": "#5A352D",
+      "title": "What category does the incident belong to?",
+      "callback_id": "category",
+      "actions" : [
+        {
+          "name": "red",
+          "text": "Red",
+          "type": "button",
+          "value": "red",
+          "style": "danger"
+        },
+        {
+          "name": "yellow",
+          "text": "Yellow",
+          "type": "button",
+          "value": "yellow"
+        },
+        {
+          "name": "green",
+          "text": "Green",
+          "type": "button",
+          "value": "green"
+        },
+        {
+          "name": "other",
+          "text": "I don't know",
+          "type": "button",
+          "value": "other"
+        }
+      ]
+    }
+  ]
+};
+
+const getDateMessage = {
+  text: "When did the incident occur? (dd-mm-yy)"
+};
+
+const getConfirmationMessage = (data) =>
+{
+  return {
+   "attachments": [
+     {
+       "color": "#36a64f",
+       "pretext": "Please confirm the incident",
+       "callback_id": "confirm",
+       "fields": [
+           {
+               "title": "Category",
+               "value": data.category,
+               "short": false
+           },
+           {
+               "title": "Date",
+               "value": data.date,
+               "short": false
+           },
+           {
+               "title": "Location",
+               "value": data.location,
+               "short": false
+           },
+           {
+               "title": "Description",
+               "value": data.description,
+               "short": false
+           },
+           {
+               "title": "Witnesses",
+               "value": data.witnesses,
+               "short": false
+           }
+         ],
+       "actions" : [
+         {
+           "name": "confirm",
+           "text": "Confirm",
+           "type": "button",
+           "value": "confirm",
+           "style": "ok"
+         },
+         {
+           "name": "cancel",
+           "text": "Cancel",
+           "type": "button",
+           "value": "cancel"
+         }
+       ]
+     }
+   ]
+ };
+}
+
+const witnessesMessage = {
+  attachments: [
+    {
+      color: '#5A352D',
+      title: 'Do you have any witnesses?',
+      callback_id: 'witnesses',
+      actions: [
+        {
+          name: 'yes',
+          text: 'Yes',
+          type: 'button',
+          value: 'yes',
         },
         {
           name: 'no',
@@ -23,7 +143,34 @@ const initiate = {
 };
 
 
+const confirmationMessage = {
+  "attachments": [
+      {
+          "color": "#36a64f",
+          "pretext": "Please confirm the incident",
+          "text": "Optional text that appears within the attachment",
+          "fields": [
+              {
+                  "title": "Priority",
+                  "value": "High",
+                  "short": true
+              }
+          ],
+          "image_url": "http://my-website.com/path/to/image.jpg",
+          "thumb_url": "http://example.com/path/to/thumb.png",
+          "footer": "Slack API",
+          "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+          "ts": 123456789
+      }
+  ]
+};
+
+
 module.exports = {
-  initiate
+  initiationMessage: initiate,
+  getDateMessage,
+  getConfirmationMessage,
+  categoryMessage,
+  witnessesMessage
 };
 
